d42dc503d8e4d52e0d05ece74838ac599090cc40
sanjit87m <sanjit87m@gmail.com>
2018-11-08 00:29:53 +0530

    Read json data from local file

--- app/src/main/java/org/bsc/com/bsc/Model.java
+++ app/src/main/java/org/bsc/com/bsc/Model.java
@@ -1,10 +1,106 @@
-package org.molina.com.molina;
+package org.bsc.com.bsc;
 
-/**
- * Created by sanjit on 08/10/18.
- */
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Model {
+    @SerializedName("Categories")
+    private ArrayList<Categories> categoriesArrayList;
+    public void setCategories(ArrayList<Categories> categoriesArrayList)
+    {
+        this.categoriesArrayList = categoriesArrayList;
+    }
 
+    public ArrayList<Categories> getCategories()
+    {
+        return categoriesArrayList;
+    }
+    public class Categories implements Serializable {
+
+        @SerializedName("title")
+        private String title;
+        @SerializedName("index")
+        private String index;
+        @SerializedName("subcategories")
+        public List<Subcategories> subcategories;
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getIndex() {
+            return index;
+        }
+
+        public void setIndex(String index) {
+            this.index = index;
+        }
+
+        public List<Subcategories> getSubcategories() {
+            return subcategories;
+        }
+
+        public void setSubcategories(List<Subcategories> subcategories) {
+            this.subcategories = subcategories;
+        }
+
+
+        public class Subcategories implements Serializable {
+            @SerializedName("title")
+            private String title;
+            @SerializedName("index")
+            private String index;
+            @SerializedName("location")
+            private String location;
+            @SerializedName("url")
+            private String url;
+            public String getTitle() {
+                return title;
+            }
+
+            public void setTitle(String title) {
+                this.title = title;
+            }
+
+            public String getIndex() {
+                return index;
+            }
+
+            public void setIndex(String index) {
+                this.index = index;
+            }
+
+            public String getLocation() {
+                return location;
+            }
+
+            public void setLocation(String location) {
+                this.location = location;
+            }
+
+            public String getUrl() {
+                return url;
+            }
+
+            public void setUrl(String url) {
+                this.url = url;
+            }
+
+
+
+        }
+
+
+
+
+    }
 
 }
+
+
